5ed22fece706ac29400d4c91c8dd9240ffd2e383
JohnGrotke <john.grotke@gmail.com>
2018-01-21 20:26:51 -0500

    messed up carosel but did backgrounds and stuff

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
-
-
+$('.carousel').carousel();
+// $('.carousel.carousel-slider').carousel({fullWidth: true});
 });
+// $('.carousel').carousel({fullWidth: true});
+
 
 function hideAll(){
     $('.main').attr('style', 'display:none');
@@ -12,23 +14,27 @@ function openSponsor(){
     console.log('sponser');
     hideAll();
     $('#sponsor').attr('style', 'display:block');
-    $('body').attr('style', "background-image: Url('')")
+    $('body').attr('style', "background-image: Url(''); background-color: #FFFFFF")
+
 }
 
 function openTeam(){
     console.log('team');
     hideAll();
-    $('#team').attr('style', 'display:block');
+    $('#team').attr('style',  'display:block');
+    $('body').attr('style', "background-image: Url(''); background-color: #444444")
 }
 
 function openProject(){
     console.log('project');
     hideAll();
     $('#project').attr('style', 'display:block');
+    $('body').attr('style', "background-image: Url(''); background-color: #999999")
 }
 
 function openHome(){
     console.log('home');
     hideAll();
-    $('#home').attr('style', 'display:block');
+    $('#home').attr('style', "display:block");
+    $('body').attr('style', "background-image: Url('/images/mainbg.jpg')");
 }
