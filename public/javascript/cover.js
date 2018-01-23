869f2d58e4df15e018e27ff6bf85685f0745d667
Arun Aruljothi <arun3454@gmail.com>
2018-01-23 14:48:14 -0500

    Added Cover BG effects

--- public/javascript/cover.js
+++ public/javascript/cover.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
     $('body').fadeIn(600);
+    $('.carousel').carousel({interval: 7000});
+
+    setInterval(function() {
+        var $body = $('body');
+        if($body.hasClass('bg1'))
+        {
+            $body.removeClass('bg1');
+            $body.addClass('bg2');
+        }
+       else {        
+            $body.removeClass('bg2');
+            $body.addClass('bg1');
+         }
+     }, 5000);
 });
 
 function changePage(page){
